72f781206fed8d8e4d92fce1e0156e2830af8fd5
dbelett98 <dbelett@outlook.com>
2025-01-04 17:48:36 -0600

    added hover effect JS

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -16,5 +16,25 @@
     }
   }
 
+  function createGrid(size) {
+    container.innerHTML = '';
+    const squareSize = 960 / size;
+  
+    for (let i = 0; i < size * size; i++) {
+      const square = document.createElement('div');
+      square.classList.add('grid-square');
+      square.style.width = `${squareSize}px`;
+      square.style.height = `${squareSize}px`;
+  
+      // Add hover effect
+      square.addEventListener('mouseover', () => {
+        square.style.backgroundColor = 'black';
+      });
+  
+      container.appendChild(square);
+    }
+  }
+  
+  
   createGrid(gridSize);
 
